c67b244efa58ccf705de4b04ff15e843e0ce10ae
AJ ONeal <coolaj86@gmail.com>
2012-10-25 23:23:50 -0600

    fixed leaky 'ok' and hoisted for loop vars

--- quickParser.js
+++ quickParser.js
@@ -1,3 +1,4 @@
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true unused:true undef:true*/
 /*!
  * formaline parser, an implementation of QuickSearch algorithm
  * http://www-igm.univ-mlv.fr/~lecroq/string/node19.html#SECTION00190
@@ -15,10 +16,18 @@
  *   (--)------boundary(--)\r\n
  *   
  */
-var log = console.log,
-    
+(function () {
+  "use strict";
+var //log = console.log,
     lookupTable = function ( p ) {
-        for( var b = new Buffer( 255 ), m = p.length, i = 255, j = 0, c = m + 1; i >= 0 ; b[ i ] = 0x00, i-- );
+      var b = new Buffer( 255 )
+        , m = p.length
+        , i = 255
+        , j = 0
+        , c = m + 1
+        ;
+
+        for( ; i >= 0 ; b[ i ] = 0x00, i-- );
         for( ; c > 0; b[ p[ j++ ] ] = --c );
         return b;
     },
@@ -28,7 +37,19 @@ var log = console.log,
     lknr = lookupTable( nrnr ),
     
     crlfcrlfMatch = function ( t, l ) { // stop at first match obviously.. l is start index
-        for ( var m = 4, n = t.length, j = l || 0, z = 0, x = nrnr[ 0 ], pos = j, y = t[ pos ], i = m, c = t[ i ]; j <= n - m; i = j + m, c = t[ i ], z = 0, ok = 1, pos = j, x = nrnr[ 0 ], y = t[ pos ] ) {
+      var m = 4
+        , n = t.length
+        , j = l || 0
+        , z = 0
+        , x = nrnr[ 0 ]
+        , pos = j
+        , y = t[ pos ]
+        , i = m
+        , c = t[ i ]
+        , ok
+        ;
+
+        for ( ; j <= n - m; i = j + m, c = t[ i ], z = 0, ok = 1, pos = j, x = nrnr[ 0 ], y = t[ pos ] ) {
             for ( ; z < m; z++, pos++, x = nrnr[ z ], y = t[ pos ] ) {
                 if ( x === y ) { continue; }
                 else { ok = 0; break; }
@@ -52,15 +73,29 @@ var log = console.log,
     qkproto = quickParser.prototype;
     
 qkproto.parse = function ( t ) {
-    var me = this,
-        p = me.p,
-        m = me.len,
-        lkb = me.lkb,
-        n = t.length,
-        result = {},
-        arr = new Array();
+    var me = this
+      , p = me.p
+      , m = me.len
+      , lkb = me.lkb
+      , n = t.length
+      , result = {}
+      , arr = []
+        // for loop vars
+      , j = 0
+      , ok = 1
+      , z = 0
+      , x = p[ 0 ]
+      , pos = 0
+      , y = t[ pos ]
+      , i = m
+      , c = t[ i ]
+      , pmatches = 0
+        // inner for loop vars
+      , hs
+      ;
+
     // Every Boundary has two "--" than normal  --(--Axc43434) end with --(--Axc43434)--
-    for ( var j = 0, ok = 1, z = 0, x = p[ 0 ], pos = 0, y = t[ pos ], i = m, c = t[ i ], pmatches = 0; j <= n - m; i = j + m, c = t[ i ], z = 0, pmatches += ok, ok = 1, pos = j, x = p[ 0 ], y = t[ pos ] ) { 
+    for ( ; j <= n - m; i = j + m, c = t[ i ], z = 0, pmatches += ok, ok = 1, pos = j, x = p[ 0 ], y = t[ pos ] ) { 
         for ( ; z < m ; z++, pos++, x = p[ z ], y = t[ pos ] ) {
              //log( '    z:', z, 'pos:', pos, 'x:', x, 'y:', y, x === y ); // matching sequence good for testing
             if ( x === y ) {
@@ -72,7 +107,7 @@ qkproto.parse = function ( t ) {
         if ( ok ) { 
             // log( 'matched BOUNDARY at:',j );
             // log( '   stripe:',t[j-5],t[j-4],t[j-3],t[j-2],t[j-1],'-',t[j],'-',t[j+1],t[j+2],t[j+3],t[j+4],t[j+5],t[j+6]);//,' j:',j );
-            var hs = crlfcrlfMatch( t, j + m + 2 ); // +2 is for  CRLF boundary\r\n
+            hs = crlfcrlfMatch( t, j + m + 2 ); // +2 is for  CRLF boundary\r\n
                 result = {
                         start : j , 
                         finish : hs || m
@@ -88,4 +123,4 @@ qkproto.parse = function ( t ) {
 
 exports.quickParser = quickParser;
 exports.parse = quickParser;
-
+}());
